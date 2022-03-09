3d17c74629a24c8737aeb4650a371031b2ccc929
baekchan1024 <baekchan1024@gmail.com>
2022-03-09 15:17:53 +0900

    ADD third chapter

--- src/main/java/com/study/springboot/web/HelloController.java
+++ src/main/java/com/study/springboot/web/HelloController.java
@@ -2,6 +2,7 @@ package com.study.springboot.web;
 
 import com.study.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +11,10 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloResponseDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
     }
 }
--- src/main/java/com/study/springboot/web/dto/HelloResponseDto.java
+++ src/main/java/com/study/springboot/web/dto/HelloResponseDto.java
@@ -11,9 +11,4 @@ public class HelloResponseDto {
 
     private final String name;
     private final int amount;
-
-    @GetMapping("/hello/dto")
-    public HelloResponseDto helloResponseDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
-        return new HelloResponseDto(name, amount);
-    }
 }
