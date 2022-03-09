15fe840b6768f9c48e4016541b133a0210c831e0
baekchan1024 <baekchan1024@gmail.com>
2022-03-09 18:31:57 +0900

    Modify .gitignore

---
+++ src/main/java/com/study/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,17 @@
+package com.study.springboot.config.auth.dto;
+
+import com.study.springboot.domain.user.User;
+import lombok.Getter;
+
+@Getter
+public class SessionUser {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
